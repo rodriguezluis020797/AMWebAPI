30c9fcb6b2038c75ac3bd197111edf615b0db00d
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-02-28 16:46:03 -0800

    Add user controller

---
+++ AMWebAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AMWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser(){
+            return new ObjectResult(StatusCode(200));
+        }
+    }
+}
