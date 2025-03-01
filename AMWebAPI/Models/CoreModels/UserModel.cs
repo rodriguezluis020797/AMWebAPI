20ef70d3bc47488e9820b240145e05e0c8ffe094
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-02-28 16:54:10 -0800

    Add user core and dto models

---
+++ AMWebAPI/Models/CoreModels/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using AMWebAPI.Models.DTOModels;
+
+namespace AMWebAPI.Models.CoreModels
+{
+    public class UserModel
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime DeleteDate { get; set; }
+
+        public void CreateNewRecordFromDTO(UserDTO dto)
+        {
+            FirstName = dto.FirstName;
+            MiddleName = dto.MiddleName;
+            LastName = dto.LastName;
+            EMail = dto.EMail;
+        }
+    }
+}
