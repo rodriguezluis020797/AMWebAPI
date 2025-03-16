e9e4205d5bc897eae601f1441abbe83855300418
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-03-16 16:25:14 -0600

    Add identity controller

---
+++ AMWebAPI/Controllers/IdentityController.cs
@@ -0,0 +1,38 @@
+﻿using AMWebAPI.Models;
+using AMWebAPI.Models.DTOModels;
+using AMWebAPI.Services.CoreServices;
+using AMWebAPI.Tools;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class IdentityController : Controller
+    {
+        private readonly IAMLogger _logger;
+        public IdentityController(IAMLogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn([FromBody] UserDTO dto)
+        {
+            _logger.LogInfo("+");
+            var response = new UserDTO();
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                response = new UserDTO();
+                response.ErrorMessage = "Server Error.";
+                response.RequestStatus = RequestStatusEnum.Error;
+            }
+            _logger.LogInfo("-");
+            return new ObjectResult(response);
+        }
+    }
+}
