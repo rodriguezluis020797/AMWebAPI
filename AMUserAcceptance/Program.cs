9dd32af5d9935c61ca932bf2693e2bbd116f0321
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-04-01 17:34:16 -0600

    Add user acceptance project.

---
+++ AMUserAcceptance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AMUserAcceptance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
