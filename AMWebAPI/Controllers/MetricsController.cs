3f4c059898c758088155182048dda66ec319bda9
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-06-04 20:09:16 -0600

    Create MetricsController.cs

---
+++ AMWebAPI/Controllers/MetricsController.cs
@@ -0,0 +1,36 @@
+using AMData.Models;
+using AMData.Models.DTOModels;
+using AMServices.CoreServices;
+using AMTools.Tools;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMWebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+[Authorize]
+public class MetricsController(IAMLogger logger, IMetricsService metricsService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> GetMetrics([FromBody] MetricsDTO dto)
+    {
+        logger.LogInfo("+");
+        try
+        {
+            var jwToken = Request.Cookies[SessionClaimEnum.JWToken.ToString()];
+            
+            var result = await metricsService.GetMetricsByRange(jwToken, dto);
+            return StatusCode((int)HttpStatusCodeEnum.Success, result);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.ToString());
+            return StatusCode((int)HttpStatusCodeEnum.ServerError);
+        }
+        finally
+        {
+            logger.LogInfo("-");
+        }
+    }
+}
